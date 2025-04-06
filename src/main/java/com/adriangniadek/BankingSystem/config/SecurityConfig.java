6c05e7a83412619eed94bf17de8f89b351ed8790
AdrianGniadek <adrian.gniadek03@gmail.com>
2025-04-06 20:30:06 +0200

    Add security

--- src/main/java/com/adriangniadek/BankingSystem/config/SecurityConfig.java
+++ src/main/java/com/adriangniadek/BankingSystem/config/SecurityConfig.java
@@ -1,12 +1,36 @@
 package com.adriangniadek.BankingSystem.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("/accounts/**", "/transfers/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasicConfigurer -> httpBasicConfigurer.realmName("Your Realm Name"))
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
